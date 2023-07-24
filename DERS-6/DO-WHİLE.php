fecff606b9eff4e7717097c48132f0986878ed5b
ethem <>
2023-07-25 01:20:18 +0300

    ...

---
+++ DERS-6/DO-WHİLE.php
@@ -0,0 +1,8 @@
+<?php
+$i=0;
+do {//BİR KERELİĞİNE MAHSUS ÇALIŞMAK ZORUNDADIR. 
+    echo $i."<br>";
+    $i++;
+} while ($i <= 10);
+
+?>
---
+++ DERS-6/FOR DÖNGÜSÜ.php
@@ -0,0 +1,12 @@
+<?php
+//FOR DÖNGÜSÜ: initialisation değişken atamasını bir kereliğine çalıştırır.
+//$i=0;=>initialisation yani değişken ataması yaptık;
+//+i<10;=>ŞART KISMI.
+//$i++=> DÖNGÜ SONSUZ DÖNGÜYE GİRMESİN DİYE. 
+for ($i=0; $i <=10 ; $i++) { 
+    ECHO "$i<br>";
+}
+
+
+
+?>
