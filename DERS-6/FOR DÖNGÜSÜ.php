baeed147a1ad2918709c59a1ec683216aae83bf9
ethem <>
2023-07-25 01:26:23 +0300

    ...

--- DERS-6/FOR DÖNGÜSÜ.php
+++ DERS-6/FOR DÖNGÜSÜ.php
@@ -6,6 +6,10 @@
 for ($i=0; $i <=10 ; $i++) { 
     ECHO "$i<br>";
 }
+$isimler=["ethem","tuğrul","araç"];
+for ($i=count($isimler)-1; $i>=0 ; $i--) { //$İ=COUNT($İSİMLER)-1;-1 koymamızın sebebi isimler arrayin 3 elemanlı olması(count fonksiyonu), en son değerin("araç") keys'i 2 ve 2'yi yazdırmak için bizim 3. elemanı değil 2. elemanı yazdırmasını istememiz gerekiyor.
+    echo $isimler[$i]."<br>";
+}
 
 
 
